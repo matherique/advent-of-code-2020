db0d9fd901cf325863e36c8c0bf88b9ff74cda14
Matheus Henrique <matherique@gmail.com>
2020-12-02 21:30:42 -0300

    feat(base): add base mode

---
+++ src/base/index.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const filepath = path.resolve(__dirname, "in");
+const data = fs.readFileSync(filepath).toString().split("\n");
+
+export function part1() {
+  let anwser = 0;
+  
+  return anwser;
+}
+
+export function part2() {
+  let anwser = 0;
+  
+  return anwser;
+}
+
